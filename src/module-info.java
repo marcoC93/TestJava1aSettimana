5a8407bc76dd533aa0d89721d3af49d3e3505688
Marco <marco.chianese93@gmail.com>
2021-04-23 10:25:48 +0200

    Setup iniziale + stampa "Hello World" di prova

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module testJava1aSettimana {
+}
---
+++ src/testJava1aSettimana/TestJava1aSettimana.java
@@ -0,0 +1,7 @@
+package testJava1aSettimana;
+
+public class TestJava1aSettimana {
+	public static void main(String[] args) {
+		System.out.println("hello world ");
+	}
+}
