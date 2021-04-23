297a0b1b126263508b9931561f819ebf6b24a880
Marco <marco.chianese93@gmail.com>
2021-04-23 11:26:04 +0200

    Calcolo perimetro triangolo rettangolo

--- src/testJava1aSettimana/TestJava1aSettimana.java
+++ src/testJava1aSettimana/TestJava1aSettimana.java
@@ -1,4 +1,4 @@
-// In questo Esercizio creeró una calcolatrice che misuri in automatico perimetro ed altezza di un triangolo
+// In questo Esercizio creeró una calcolatrice che misuri in automatico perimetro ed altezza di un triangolo rettangolo conoscendo i lati
 
 package testJava1aSettimana;
 
@@ -7,7 +7,10 @@ import java.util.Scanner;
 public class TestJava1aSettimana {
 	public static void main(String[] args) {
 		
+//		 qui vado a richiamare il metodo scanner in modo da poterlo utilizzare in seguito per l'input dei dati
+	
 		Scanner s = new Scanner(System.in);
+//		Chiedo all'utente di inserire i vari  dati per effettuare il calcolo 
 		
 		System.out.println("Inserisci la base");
 		int baseInserita = s.nextInt();
@@ -16,9 +19,28 @@ public class TestJava1aSettimana {
 		int altezzaInserita = s.nextInt();
 		
 		
+//		chiedo l'ipotenusa 
+		System.out.println("Inserisci l'ipotenusa ");
+		int ipotenusaInserita = s.nextInt();
+		
+//		Qui faccio stampare a schermo 
 		System.out.println("base: " + baseInserita);
 		
+		
 		System.out.println("altezza: " + altezzaInserita);
+		
+		System.out.println("ipotenusa: " + ipotenusaInserita);
+		
+		
+//		qui avviene l'operazione di calcolo del perimetro 
+		
+		int perimetro = baseInserita + altezzaInserita + ipotenusaInserita ;
+		
+		
+//		Risultato finale
+		System.out.println("Perimetro = " + perimetro);
+		
+		
 		
 	}
 }
