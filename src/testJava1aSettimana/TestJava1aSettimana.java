f07dc640951db73772275d32c05b92c981d58ae3
Marco <marco.chianese93@gmail.com>
2021-04-23 10:42:55 +0200

    Inserimento dati in input da parte dell'utente

--- src/testJava1aSettimana/TestJava1aSettimana.java
+++ src/testJava1aSettimana/TestJava1aSettimana.java
@@ -1,7 +1,24 @@
+// In questo Esercizio creeró una calcolatrice che misuri in automatico perimetro ed altezza di un triangolo
+
 package testJava1aSettimana;
+
+import java.util.Scanner;
 
 public class TestJava1aSettimana {
 	public static void main(String[] args) {
-		System.out.println("hello world ");
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Inserisci la base");
+		int baseInserita = s.nextInt();
+		
+		System.out.println("Inserisci l'altezza ");
+		int altezzaInserita = s.nextInt();
+		
+		
+		System.out.println("base: " + baseInserita);
+		
+		System.out.println("altezza: " + altezzaInserita);
+		
 	}
 }
